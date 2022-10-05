75f416ef7b9de7175e9c74ce83baa84653d8ba0d
Jay-Ubisse <joaquimubisse@gmail.com>
2022-10-05 12:25:52 +0200

    configurando o Tailwindcss

--- index.php
+++ index.php
@@ -4,9 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Binario Store</title>
+    <title>Loja - Binario</title>
+    <link rel="stylesheet" href="./assets/styles/output.css">
 </head>
 <body>
-
+    <h1 class="text-4xl text-blue-700">
+        Página Inicial
+    </h1>
 </body>
 </html>
